2869dd4f8d84c421d96ad04b279c8eb094686773
Jon Zadra <jon.zadra@sorensonimpact.com>
2020-03-11 15:20:54 -0600

    initial commit

---
+++ R/misc/hexsticker.R
@@ -0,0 +1,14 @@
+require(tidyverse)
+require(siverse)
+require(hexSticker)
+
+img <- "https://www.naicu.edu/getmedia/4c12bbda-f945-4b04-81ea-6065474e69c0/IPEDS-2.png"
+
+sticker(img, package = "tidyipeds", filename = "~/temp/hexsticker.png",
+        h_fill = "white",
+        p_color = "black",
+        p_family = "Roboto",
+        h_color = "gold",
+        s_x = 1,
+        s_y = .75,
+        s_width = .5)
